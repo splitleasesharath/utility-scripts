76e4242a2c57d2c6499090824af9888d4ae5a8c5
Ahsan Ahmad <splitleaseahsan@gmail.com>
2025-01-28 09:46:56 -0600

    retrieve address

--- reverse_geocoding.py
+++ reverse_geocoding.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,18 +13,24 @@ HEREMAP_API_HEY = os.getenv('HEREMAP_API_HEY')
 def get_human_readable_address( ):
 
         lat, lng = '41.85527', '-87.63752'
+        lat, lng = '31.5204', '74.3587'
+        # lat, lng = '34.052235', '-118.243683'
+        # lat, lng = '29.760427', '-95.369804'
         nearby_url = 'https://api.tomtom.com/search/2/nearbySearch/.json'
         nearby_params = {
             'key': TOMTOM_API_KEY,
             'lat': lat,
             'lon': lng,
             'radius': '500',
-            'limit': 50,
+            'limit': 1,
         }
 
         nearby_response = requests.get(nearby_url, params=nearby_params)
         nearby_response.raise_for_status()
         places = nearby_response.json()
+
+        with open("tomtom_result.json", "w") as file:
+            json.dump(places, file, indent=4)
         print( places)
 
 
@@ -31,14 +38,41 @@ def get_human_readable_address( ):
         params = {
             "at": f"{lat},{lng}",
             "lang": "en-US",  # Language for the response
-            "apikey": HEREMAP_API_HEY
+            "apikey": HEREMAP_API_HEY,
+            "mode":'retrieveAddresses'
         }
 
 
         response = requests.get(url, params=params)
         response.raise_for_status()
         data = response.json()
-        print(data)
+      
+
+        if data.get("items"):
+                address = data["items"][0].get("address", {})
+                city = address.get("city", "")
+                state = address.get("state", "")
+
+                # Build the location string
+                if city and state:
+                    location = f"{city}, {state}"
+                elif city:
+                    location = city
+                elif state:
+                    location = state
+                else:
+                    location = "Location not found"
+
+                
+                print(f"Lat/Lng: {lat},{lng} => Location: {location}")
+        else:
+            print(f"Lat/Lng: {lat},{lng} => No address found.")
+            
+
+
+       
+        with open("here_result.json", "w") as file:
+            json.dump(data, file, indent=4)
 
 if __name__ == "__main__":
     get_human_readable_address()
